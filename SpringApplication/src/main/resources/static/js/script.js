0187cc5b6c759108c48e9ac56d5789dbe409c27c
eglab <01169284@pw.edu.pl>
2022-12-29 19:02:30 +0100

    #Praca nad stroną powitalną(plik index.html, stworzenie niezbednych plikow css w folderze css)

--- SpringApplication/src/main/resources/static/js/script.js
+++ SpringApplication/src/main/resources/static/js/script.js
@@ -1,3 +1,15 @@
 function successfullyLogout() {
     alert("You have been successfully logged out");
 }
+
+
+
+/**<script type="text/javascript" style="text-align: center;color: red;font-size: 30px;height: 30px;width: 30px">
+    var today = new Date();
+    var day = today.getDay();
+    var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date
+    document.getElementById("displayDateTime").innerHTML = dateTime + ' ' +daylist[day];
+</script>**/
